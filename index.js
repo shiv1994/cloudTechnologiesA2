86494104e490747311d953258ba68567b431c0a9
Shiva <shivramx@gmail.com>
2017-10-17 14:54:48 -0700

    Added models, UI working and pushign to server.

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
 var express = require('express')
-var app = express()
-var router = express.Router();
+
 var bodyParser = require('body-parser')
+
+var router = express.Router();
+
+var app = express();
 
 var path = __dirname + '/views/';
 
-app.use('/', router);
+var events = [];
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-var events = {};
 var eventId = 0;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json()); 
+
 router.get('/',function(req, res){
-  res.sendFile(path + 'index.html');
+    res.sendFile(path + 'index.html');
 });
 
-router.post('/saveEvent', function (req, res) {
-  //  var person = req.param('PersonSelect');
-  //  var theatre = req.param('TheatreSelct');
-  //  var numTickets = req.param('NumTicksSelect');
-  //  var event = {'person':person, 'theatre':theatre, 'numTickets':numTickets};
-  //  events.eventId = event;
-  //  eventId++;
-   console.log(req.params);
-   console.log(req.body);
-})
+//Types of Events: Add Movie Sale, Remove Movie Sale, Possibly Add Movie Ticket Sellers.
+
+router.post('/saveEvent', function (req, res){
+    var person = req.body.PersonSelect;
+    var theatre = req.body.TheatreSelct;
+    var numTickets = req.body.NumTicksSelect;
+    var transType = req.body.TransType;
+    var event = {'person':person, 'theatre':theatre, 'numTickets':numTickets, 'transType':transType};
+    events.push(event);
+    eventId++;
+    res.sendFile(path + 'index.html');
+    console.log(events);
+});
+
+app.use('/', router);
 
 app.listen(8000, function () {
-  console.log('Example app listening on port 8000!');
-})
+    console.log('Example app listening on port 8000!');
+});
+
+function processEvents(){
+    events.forEach(function(element) {
+        //Firstly need to check from the last snapshot point, if any exists.
+        //If it does exist, we can start from that particular snapshot.
+        //We may model the snapshot like this: snapshot={snapshotId, theater:{t1:500, t2:1000}, personsSelling:{Names of Persons}}
+    }, this);
+}
+
+setInterval(processEvents, 10000);
---
+++ models/classes.js
@@ -0,0 +1,20 @@
+class SalesPerson {
+    constructor(fName, lName) {
+      this.firstName = fName;
+      this.lastName = lName;
+    }
+  }
+
+class Theatre {
+  constructor(number, movieShowing) {
+      this.number = number;
+      this.movieShowing = movieShowing;
+  }
+}
+
+class Sale{
+  constructor(numTickets, saleType) {
+    this.numTickets = numTickets;
+    this.saleType = saleType;
+  }
+}
